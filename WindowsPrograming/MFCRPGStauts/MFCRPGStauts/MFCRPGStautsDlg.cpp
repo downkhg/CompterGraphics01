641cc5d9f24d1f7e6c5c188e1e9133465df0c22d
Administrator <Administrator@DESKTOP-4UNEB7C>
2024-07-01 13:53:28 +0900

    MFC구조 분석 및 객체지향의 이해

---
+++ WindowsPrograming/MFCRPGStauts/MFCRPGStauts/MFCRPGStautsDlg.cpp
@@ -0,0 +1,173 @@
+﻿
+// MFCRPGStautsDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCRPGStauts.h"
+#include "MFCRPGStautsDlg.h"
+#include "afxdialogex.h"
+#include <conio.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCRPGStautsDlg 대화 상자
+
+
+
+CMFCRPGStautsDlg::CMFCRPGStautsDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCRPGSTAUTS_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCRPGStautsDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_BUTTON1, m_btnButton);
+}
+
+BEGIN_MESSAGE_MAP(CMFCRPGStautsDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCRPGStautsDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CMFCRPGStautsDlg 메시지 처리기
+
+BOOL CMFCRPGStautsDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	m_btnButton.SetWindowTextW(_T("OnInitDialog"));
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	AllocConsole();
+	_cprintf("OnInitDialog\n");
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCRPGStautsDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	_cprintf("OnSysCommand\n");
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+		m_btnButton.SetWindowTextW(_T("IDM_ABOUTBOX"));
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+		m_btnButton.SetWindowTextW(_T("OnSysCommand"));
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCRPGStautsDlg::OnPaint()
+{	
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+		_cprintf("OnPaint\n");
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCRPGStautsDlg::OnQueryDragIcon()
+{
+	_cprintf("OnQueryDragIcon\n");
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCRPGStautsDlg::OnBnClickedButton1()
+{
+	_cprintf("OnBnClickedButton1\n");
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	//m_btnButton.SetWindowTextW(_T("Button1 Click!!"));
+}
---
+++ WindowsPrograming/MFCRPGStauts/MFCRPGStauts/MFCRPGStautsDlg.h
@@ -0,0 +1,38 @@
+﻿
+// MFCRPGStautsDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCRPGStautsDlg 대화 상자
+class CMFCRPGStautsDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCRPGStautsDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCRPGSTAUTS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	// 버튼을 누르면 버튼의 텍스트를 변경하게 만든다.
+	CButton m_btnButton;
+	afx_msg void OnBnClickedButton1();
+};
