3bc84a2af9d5b381816bcb08d62a9bc3315820ac
Administrator <Administrator@DESKTOP-4UNEB7C>
2024-07-02 13:07:29 +0900

    큐잉을 이용한 씹힙현상 해결

--- WindowsPrograming/WinAPIDemo/WinAPIDemo/main.cpp
+++ WindowsPrograming/WinAPIDemo/WinAPIDemo/main.cpp
@@ -1,15 +1,16 @@
 /*##################################
 스레드를 이용한 WinAPI데모(수업용)
-파일명: WinAppDemo.cpp
+파일명: WinAppDemo_Queueing.cpp
 작성자: 김홍규 (downkhg@gmail.com)
-마지막수정날짜: 2020.12.14
-버전: 1.2 (메세지 주소값 출력 및 문제 단계별화)
+마지막수정날짜: 2020.12.15
+버전: 1.0 (정답공개용)
 ###################################*/
 
 #include <iostream>
 #include <Windows.h>
 #include <process.h>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -21,35 +22,46 @@ bool g_bLoop = true;
 //arg를 통해 외부의 데이터값을 받을수있다.
 unsigned int WINAPI WndProc(void* arg)
 {
-	cout << "arg:" << arg << endl;
-	int* pData = (int*)arg;
+	cout << "WndProc Start - arg:" << arg << endl;
+	queue<int>* que = (queue<int>*)arg;
 
+	//int* pData = (int*)arg;
+	int nCount = 0;
 	while (g_bLoop)
 	{
-		switch (*pData)
+		cout << "Thread Loop While:" << nCount << endl;
+		if (que->empty() == false)
 		{
-		case CREATE:
-			cout << "초기화" << endl;
-			*pData = COMMOND;
-			break;
-		case COMMOND:
-			cout << "명령을 입력하세요." << endl;
-			for (int i = 0; i < MAX; i++)
-				cout << i << ":" << strMSG[i];
-			cout << endl;
-			break;
-		case PAINT:
-			cout << "화면에 그립니다." << endl;
-			break;
-		case DESTROY:
-			cout << "프로그램을 종료합니다." << endl;
-			g_bLoop = false;
-			break;
-		default:
-			break;
+			int nMsg = que->front();
+			cout << "Queue:" << nMsg << endl;
+			switch (nMsg)
+			{
+			case CREATE:
+				cout << "초기화" << endl;
+				nMsg = COMMOND;
+				break;
+			case COMMOND:
+				cout << "명령을 입력하세요." << endl;
+				for (int i = 0; i < MAX; i++)
+					cout << i << ":" << strMSG[i];
+				cout << endl;
+				break;
+			case PAINT:
+				cout << "화면에 그립니다." << endl;
+				break;
+			case DESTROY:
+				cout << "프로그램을 종료합니다." << endl;
+				g_bLoop = false;
+				break;
+			default:
+				break;
+			}
+			que->pop();
+			Sleep(2000);
+			nCount++;
 		}
-		Sleep(2000);
 	}
+	cout << "WndProc End - arg:" << arg << endl;
 	return 0;
 }
 
@@ -57,10 +69,12 @@ unsigned int WINAPI WndProc(void* arg)
 //큐를 활용하여 메세지를 큐에 쌓고, 쓰레드에서 큐에서 데이터를 1개씩꺼내서 처리하는 프로그램으로 만들기.
 //1. 큐를 전역변수로 만들어서 처리하기.
 //2. 큐를 main의 지역변수로 만들어서 동일한 과정이 가능하도록 처리하기.
+
 int main()
 {
 	HANDLE hThread = NULL;
 	DWORD dwThreadID = NULL;
+	queue<int> queMsg;
 
 	int nMSG = CREATE;
 	cout << "Msg:" << &nMSG << endl;
@@ -71,12 +85,13 @@ int main()
 	//콜백함수: 프로세스내에서 호출하지않고, 외부에서 호출하도록 하는 함수.
 	hThread = (HANDLE)_beginthreadex(NULL, 0,
 		WndProc,
-		(void*)&nMSG, 0,
+		(void*)&queMsg, 0,
 		(unsigned int*)dwThreadID);
 
 	while (g_bLoop)
 	{
 		scanf("%d", &nMSG);
+		queMsg.push(nMSG);
 	}
 
 	return 0;
