f4f7eafc42a131b26a26efd6b48eaeb4624b1cc2
Administrator <Administrator@DESKTOP-4UNEB7C>
2024-07-05 13:36:41 +0900

    클래스의 정의,선언, 멤버함수의 정의가 필요한 이유

--- DesignPattern/4.State/4.State/State.cpp
+++ DesignPattern/4.State/4.State/State.cpp
@@ -1,53 +1,71 @@
 #include <iostream>
 //객체의 선언을 해서 대상의 참조자를 활용할수 있음.
 //그러나 각 함수의 정의시에 각 객체가 선언되지않아, 컴파일 오류가 발생함.
+
+//클래스의 선언: 해당 클래스가 뒤에 정의된다.
+//선언만 된 클래스는 참조자나 포인터만 생성가능하다.
 class StateTow;
 class StateThree;
 
+//클래스의 선언: 함수의 형태를 알수 있는 정보
+//클래스의 선언만 있는 경우, 구체적으로 함수를 호출하여 사용할때 컴파일오류가발생한다.
 class State
 {
-	void GoNext(Context* context)
-	{
-		context->GoNext();
-	}
+public:
+	virtual void GoNext(Context* context) = 0;
 };
 
-class StateTow
+class StateTow : public State
 {
-	void GoNext(Context* context)
-	{
-		context->SetState(new StateThree);
-	}
+public:
+	void GoNext(Context* context);
 };
 
 class StateOne : public State
 {
-	void GoNext(Context* context)
-	{
-		context->SetState(new StateTow);
-	}
+public:
+	void GoNext(Context* context);
 };
 
-class StateThree
+class StateThree : public State
 {
-	void GoNext(Context* context)
-	{
-		context.SetState(new StateOne);
-	}
+public:
+	void GoNext(Context* context);
 };
 
 class Context
 {
 	State* m_pState;
 public:
-	void SetState(State* state)
-	{
-		m_pState = state;
-	}
-
-	void GoNext()
-	{
-		m_pState->GoNext(this);
-	}
+	void SetState(State* state);
+	void GoNext();
 };
 
+//클래스멤버함수의 정의: 선언된 클래스의 멤버함수를 정의하여 사용 가능하도록 한다.
+//정의될때 실제 선언된 함수가 존제 해야 사용가능하다.
+void StateThree::GoNext(Context* context)
+{
+	context->SetState(new StateOne);
+}
+
+void StateTow::GoNext(Context* context) 
+{
+	context->SetState(new StateThree);
+}
+;
+
+void StateOne::GoNext(Context* context)
+{
+	context->SetState(new StateTow);
+}
+
+
+void Context::SetState(State* state)
+{
+	m_pState = state;
+}
+
+void Context::GoNext()
+{
+	m_pState->GoNext(this);
+}
